82f7e0a53698313440a801dd9fd66a09e723677f
Jeff Jijy <jeffjijy@yahoo.com>
2024-06-11 11:20:11 -0400

    my changes

--- Terraform/Main.tf
+++ Terraform/Main.tf
@@ -11,14 +11,61 @@ terraform{
 
 provider "aws" {
     region = "us-west-2"
-    access_key = "AKIA5FTZFKFZBDAD3KXY"
-    secret_key="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"
+    access_key = var.aws_access_key_id
+    secret_key = var.aws_secret_access_key
 }
 
 resource "aws_instance" "app_server" {
     ami = "ami-830c94e3"
     instance_type = "t2.micro"
+    key_name = "terraform-key-pairs"
+    vpc_security_group_ids = ["sg-0d6473f814374a9a6"]
     tags = {
         Name = "ExampleAppServerInstance"
     }
+}
+
+ provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "mkdir project",
+      "cd project",
+      "sudo yum install git -y",
+      "git clone https://github.com/jmathew19/test.git",
+      "cd react-aws-terraform-project",
+      "sudo yum install -y nodejs npm",
+      "node --version",
+      "npm --version",
+      "npm install web-vitals",
+      "npm install react-scripts --save-dev",
+      "npm install react-dom",
+      "npm install react-router-dom",
+      "cd ~/project/test",
+      "npm start"
+      # Additional commands to start your application
+
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = base64decode(var.private_key_base64)
+      host        = self.public_ip
+    }
+  }
+}
+
+variable "private_key_base64" {
+  description = "Base64 encoded private key content"
+  type        = string
+}
+
+variable "aws_access_key_id" {
+  description = "AWS access key ID"
+  type        = string
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS secret access key"
+  type        = string
 }
