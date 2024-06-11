ff51deaa27b0bb7f4c0a977b12e9077dd2ec2495
Jeff Jijy <jeffjijy@yahoo.com>
2024-06-11 12:47:48 -0400

    reeee4

--- helloworld/class HelloWorld{.java
+++
@@ -1,8 +0,0 @@
-class HelloWorld{
-    public static void main(String{} args) {
-        System.out.printIn("Hello Sreyo!");
-        //First Prompt
-        System.out.print("My name is jeff")
-        //here is my name
-    }
-    
