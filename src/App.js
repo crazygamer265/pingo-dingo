0e20bf6c093788fbefd159644c7d607da9982c0b
Jeff Jijy <jeffjijy@yahoo.com>
2024-06-25 11:43:39 -0400

    sdvdzzzzz

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> shakoobbab
+          Edit <code>src/App.js</code> shakoobba
         </p>
         <a
           className="App-link"
